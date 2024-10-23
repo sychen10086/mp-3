8fcaa3c1cd0668cf2b00f99078a4fb2e95ce0aaf
sychen10086 <123329572+sychen10086@users.noreply.github.com>
2024-10-22 23:33:52 -0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,7 @@ import styles from './style.module.css';
 
 const Header: React.FC = () => {
     return (
-        <header className={styles.header}>
-            
-        </header>
+        <header className={styles.header}></header>
     );
 };
 
