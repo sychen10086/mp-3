3fe029ab1c493f9ccf67220e1246d518b7a707d3
sychen10086 <123329572+sychen10086@users.noreply.github.com>
2024-10-22 23:46:20 -0400

    Update Appreciations.tsx

--- src/pages/Appreciations.tsx
+++ src/pages/Appreciations.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Appreciation: React.FC = () => {
     return (
         <main>
-            <img src="./photos/thanks_card.jpg" id="thankscard" alt="Thanks card" />
+            <img src="photos/thanks_card.JPG" id="thankscard" alt="Thanks card" />
             <h2>Nice to Meet You Again </h2>
         </main>
     );
