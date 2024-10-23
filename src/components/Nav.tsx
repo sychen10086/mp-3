985b222d7ba4d68b745b6a120946eabbfdb8c5ea
sychen10086 <123329572+sychen10086@users.noreply.github.com>
2024-10-22 23:19:15 -0400

    Update Nav.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Header: React.FC = () => {
+const Nav: React.FC = () => {
     return (
-        <header>
-            <h1>Thank You For Your Time!</h1>
-        </header>
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/education">Education</Link></li>
+                <li><Link to="/experience">Experience</Link></li>
+                <li><Link to="/gamedesign">Game Design</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+                <li><Link to="/appreciation">Appreciation</Link></li>
+            </ul>
+        </nav>
     );
 };
 
-export default Header;
+export default Nav;
 
