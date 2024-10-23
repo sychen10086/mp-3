c527344e786f2ae4ec2b3702902caf233bcbcd1a
sychen10086 <123329572+sychen10086@users.noreply.github.com>
2024-10-22 23:53:38 -0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home: React.FC = () => {
     return (
         <main>
-            <img src="photos/myself.JPG" id="myself" alt="Myself" />
+            <img src="photos/thanks_card.JPG" id="myself" alt="Myself" />
             <h2>Welcome to my Resume, my name is Shangyuan Chen.</h2>
             <p>Email: sychen@bu.edu & Phone: 617-763-8742</p>
         </main>
